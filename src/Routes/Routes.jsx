8bad9644a58bc946c206869faeb13bb97c16ffef
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-11-04 21:31:07 +0600

    Navbar Added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Root from "../layouts/Root";
+import Alljobs from "../Pages/AllJobs/Alljobs";
+import Blogs from "../Pages/Blogs/Blogs";
+import Login from "../Pages/Login/Login";
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "allJobs",
+        element: <Alljobs></Alljobs>,
+      },
+      {
+        path: "blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+export default Routes;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const navLink = (
+    <>
+      <li className="capitalize font-medium text-[#0F2239] text-base">
+        <NavLink className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "text-[#0d6efd] underline font-bold" : "")} to="/">
+          Home
+        </NavLink>
+      </li>
+      <li className="capitalize font-medium text-[#0F2239] text-base">
+        <NavLink
+          className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "text-[#0d6efd] underline font-bold" : "")}
+          to="/allJobs"
+        >
+          All Jobs
+        </NavLink>
+      </li>
+      <li className="capitalize font-medium text-[#0F2239] text-base">
+        <NavLink className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "text-[#0d6efd] underline font-bold" : "")} to="/blogs">
+          Blogs
+        </NavLink>
+      </li>
+      <li className="capitalize font-medium text-[#0F2239] text-base">
+        <NavLink className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "text-[#0d6efd] underline font-bold" : "")} to="/login">
+          Login
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="max-w-[1280px] mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {navLink}
+            </ul>
+          </div>
+          <NavLink to="/"><a className="text-3xl uppercase font-extrabold text-[#0d6efd]">Job Hunter</a></NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex navbar-end">
+          <ul className="menu menu-horizontal px-1">{navLink}</ul>
+          {/* <a className="btn">Button</a> */}
+        </div>
+        {/* <div className="navbar-end">
+         
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
